dba50ea8d8c2a33490fb07c3730f5a3476e40a03
Tina Huynh <tinamchuynh@gmail.com>
2024-11-23 21:49:06 -0800

    create details page for websites

--- src/app/websites/[name]/page.tsx
+++ src/app/websites/[name]/page.tsx
@@ -24,8 +24,8 @@ export default function WebsiteTypes() {
     const website = website_types[parseInt( decodeUrlSafeBase64( index! ) )];
 
     return (
-        <div className="py-10 lg:py-52 px-6 mt-16 lg:px-8 h-max max-w-4xl mx-auto">
-            <div className="text-center mb-12">
+        <div className="py-10 lg:py-52 px-6 mt-16 lg:px-8 h-max">
+            <div className="text-center mb-12 max-w-4xl mx-auto">
                 <h6>Flexible Payment Options</h6>
                 <h1>{website.name}</h1>
 
@@ -46,22 +46,94 @@ export default function WebsiteTypes() {
 
                 <h2>{website.introduction}</h2>
             </div>
-            <div className="max-w-3xl mx-auto">
-                <div className="p-8 space-y-4">
-                    <div>
-                        <h3>Features</h3>
-                        <ul>
-                            {website.other_info.features.map( ( feature, index ) => (
-                                <li key={index} className="flex gap-x-2">
+            <div className="max-w-6xl mx-auto">
+                <div className="grid grid-cols-2 lg:grid-cols-3 items-start p-8 gap-6">
+                    <div className="h-full">
+                        <h3>Who</h3>
+                        <ul className="space-y-1">
+                            {website.who.map( ( item, index ) => (
+                                <li key={index} className="flex items-center gap-x-2">
                                     <CheckIcon
                                         className={classNames(
-                                            website.popular
-                                                ? "text-deepTeal-300"
+                                            isDarkMode
+                                                ? "text-deepBlue-200"
                                                 : "text-deepTeal-600",
                                             "h-5 w-8 flex-none"
                                         )}
                                     />
-                                    {feature}
+                                    {item}
+                                </li>
+                            ) )}
+                        </ul>
+                    </div>
+                    <div className="h-full">
+                        <h3>Occasions</h3>
+                        <ul className="space-y-1">
+                            {website.occasions.map( ( item, index ) => (
+                                <li key={index} className="flex items-center gap-x-2">
+                                    <CheckIcon
+                                        className={classNames(
+                                            isDarkMode
+                                                ? "text-deepBlue-200"
+                                                : "text-deepTeal-600",
+                                            "h-5 w-8 flex-none"
+                                        )}
+                                    />
+                                    {item}
+                                </li>
+                            ) )}
+                        </ul>
+                    </div>
+                    <div className="h-full">
+                        <h3>Benefits</h3>
+                        <ul className="space-y-1">
+                            {website.other_info.benefits.map( ( item, index ) => (
+                                <li key={index} className="flex items-center gap-x-2">
+                                    <CheckIcon
+                                        className={classNames(
+                                            isDarkMode
+                                                ? "text-deepBlue-200"
+                                                : "text-deepTeal-600",
+                                            "h-5 w-8 flex-none"
+                                        )}
+                                    />
+                                    {item}
+                                </li>
+                            ) )}
+                        </ul>
+                    </div>
+                    <div className="h-full">
+                        <h3>Features</h3>
+                        <ul className="space-y-1">
+                            {website.other_info.features.map( ( item, index ) => (
+                                <li key={index} className="flex items-center gap-x-2">
+                                    <CheckIcon
+                                        className={classNames(
+                                            isDarkMode
+                                                ? "text-deepBlue-200"
+                                                : "text-deepTeal-600",
+                                            "h-5 w-8 flex-none"
+                                        )}
+                                    />
+                                    {item}
+                                </li>
+                            ) )}
+                        </ul>
+                    </div>
+                    <div className="h-full">
+                        <h3>Project Requirements</h3>
+                        <ul className="space-y-1">
+                            {website.other_info.requirements.map( ( item, index ) => (
+                                <li key={index} className="flex items-center gap-x-2">
+                                    <CheckIcon
+                                        className={classNames(
+                                            isDarkMode
+                                                ? "text-deepBlue-200"
+                                                : "text-deepTeal-600",
+                                            "h-5 w-8 flex-none"
+                                        )}
+                                    />
+                                    {item}
                                 </li>
                             ) )}
                         </ul>
