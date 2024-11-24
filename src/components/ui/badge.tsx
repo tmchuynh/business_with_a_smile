d3631401313a81e1fcd2a3fdbc0682bd67673d2b
Tina Huynh <tinamchuynh@gmail.com>
2024-11-23 21:29:07 -0800

    update badge style

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -3,18 +3,20 @@ import { cva, type VariantProps } from "class-variance-authority";
 import * as React from "react";
 
 const badgeVariants = cva(
-    "inline-flex items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
+    "inline-flex items-center rounded-md border px-4 py-1 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
     {
         variants: {
             variant: {
                 default:
-                    "border-transparent bg-deepTeal-500 text-softNeutral-50 shadow hover:bg-deepTeal-400",
+                    "border-transparent bg-deepTeal-500 text-softNeutral-50 shadow-md dark:bg-deepBlue-500",
                 secondary:
-                    "border-transparent bg-deepBlue-500 text-softNeutral-50 hover:bg-deepBlue-400",
+                    "border-transparent bg-deepBlue-500 text-softNeutral-50 shadow-md",
                 destructive:
-                    "border-transparent bg-richRed-500 text-softNeutral-50 shadow hover:bg-richRed-400",
+                    "border-transparent bg-richRed-500 text-softNeutral-50 shadow-md",
+                ghost:
+                    "text-accentGold-500 bg-transparent shadow-md",
                 outline:
-                    "border-softNeutral-300 text-softNeutral-800 hover:border-softNeutral-500",
+                    "border-softNeutral-300 dark:text-softNeutral-100 text-softNeutral-800 shadow-md shadow-deepBlue-400/40",
             },
         },
         defaultVariants: {
