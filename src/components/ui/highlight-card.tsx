9650e8e4b9b421ec59ac9e1d209c016f4d0261e0
Tina Huynh <tinamchuynh@gmail.com>
2024-11-25 13:41:15 -0800

    simplify Icons

--- src/components/ui/highlight-card.tsx
+++ src/components/ui/highlight-card.tsx
@@ -1,16 +1,15 @@
 "use client";
 import { useTheme } from 'next-themes';
 import { useRouter } from 'next/navigation';
-import React, { useEffect, useState } from 'react';
+import React, { ForwardRefExoticComponent, RefAttributes, useEffect, useState } from 'react';
 import { Button } from './button';
+import { LucideProps } from 'lucide-react';
 
 interface HighlightedCardProps {
     title: string;
-    subtitle: string;
     description: string;
-    imageUrl: string;
-    imageAlt: string;
-    actionText: string;
+    actionText?: string;
+    Icon: ForwardRefExoticComponent<Omit<LucideProps, "ref"> & RefAttributes<SVGSVGElement>>;
     onActionClick?: () => void;
     stats: {
         label: string;
@@ -20,11 +19,9 @@ interface HighlightedCardProps {
 
 const HighlightedCard: React.FC<HighlightedCardProps> = ( {
     title,
-    subtitle,
     description,
-    imageUrl,
-    imageAlt,
     actionText,
+    Icon,
     onActionClick,
     stats,
 } ) => {
@@ -39,9 +36,11 @@ const HighlightedCard: React.FC<HighlightedCardProps> = ( {
 
     return (
         <div className="relative border-2 border-neutral-100 shadow-lg rounded-lg mt-16 mx-7 pt-10 pb-4 transition-all duration-200 ease-in-out hover:shadow-xl">
-            <div className="profile__image -mt-20 mx-auto w-24 h-24 rounded-full border-4 border-deepTeal-400 dark:border-deepBlue-400 shadow-lg">
-                <img src={imageUrl} alt={imageAlt} className="object-cover w-full h-full rounded-full" />
-            </div>
+            {Icon && (
+                <div className="flex justify-center items-center bg-white -mt-20 mx-auto w-24 h-24 rounded-full border-4 border-deepTeal-400 dark:border-deepBlue-400 shadow-lg">
+                    <Icon className="h-10 w-10 text-deepTeal-500 dark:text-teal-40" />
+                </div>
+            )}
             <div className="profile__info text-center mt-6 px-6">
                 <h3>{title}</h3>
                 <p className='w-8/12 mx-auto'>{description}</p>
@@ -55,13 +54,15 @@ const HighlightedCard: React.FC<HighlightedCardProps> = ( {
                 ) )}
             </div>
             <div className="profile__cta mt-6 px-6">
-                <Button
-                    onClick={onActionClick}
-                    className="w-1/4 mx-auto"
-                    variant={isDarkMode ? "secondary" : "outline"}
-                >
-                    {actionText}
-                </Button>
+                {mounted && (
+                    <Button
+                        onClick={onActionClick}
+                        className="w-1/4 mx-auto"
+                        variant={isDarkMode ? "secondary" : "outline"}
+                    >
+                        {actionText}
+                    </Button>
+                )}
             </div>
         </div>
     );
